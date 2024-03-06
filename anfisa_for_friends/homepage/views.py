4180f65abca69ffed6b657350a64760a30057fbb
Oleg Bryukhanov <olegbrukhanov@yandex.ru>
2024-03-06 11:57:13 +0300

    ice_cream views

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -6,17 +6,13 @@ from ice_cream.models import IceCream
 
 
 def index(request):
-    template_name = 'homepage/index.html'
+    template = 'homepage/index.html'
     ice_cream_list = IceCream.objects.values(
-        'id',
-        'title',
-        'description',
+        'id', 'title', 'description'
     ).filter(
-        Q(is_published=True)
-        & (Q(is_on_main=True)
-           | Q(title__contains='пломбир'))
-    )
+        is_published=True, is_on_main=True
+    ).order_by('title')[1:4]
     context = {
         'ice_cream_list': ice_cream_list,
     }
-    return render(request, template_name, context)
+    return render(request, template, context)
